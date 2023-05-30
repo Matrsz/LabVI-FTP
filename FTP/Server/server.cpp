600ec451fab0b7ecb258288a405f60ccfb0fd5fc
Matías Roqueta <matias.roqueta.sz@gmail.com>
2023-05-30 15:28:39 -0300

    May30-1

--- FTP/Server/server.cpp
+++ FTP/Server/server.cpp
@@ -3,93 +3,109 @@
 #include <unistd.h>
 #include <iostream>
 #include <cstring>
+#include <dirent.h>
+
+int createSocket(int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        std::cerr << "Failed to create socket." << std::endl;
+        return -1;
+    }
+
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Failed to bind socket." << std::endl;
+        close(sockfd);
+        return -1;
+    }
+
+    if (listen(sockfd, 1) == -1) {
+        std::cerr << "Failed to listen on socket." << std::endl;
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
+
+int acceptClientConnection(int socket) {
+    sockaddr_in clientAddress{};
+    socklen_t clientAddressLength = sizeof(clientAddress);
+    int clientSocket = accept(socket, (struct sockaddr*)&clientAddress, &clientAddressLength);
+    if (clientSocket == -1) {
+        std::cerr << "Failed to accept client connection." << std::endl;
+        close(socket);
+        return -1;
+    }
+
+    return clientSocket;
+}
+
+void sendWelcomeMessage(int socket, const std::string& dataAddress, int dataPort) {
+    std::string welcomeMessage = "220 Welcome to the FTP server.\r\n";
+    send(socket, welcomeMessage.c_str(), welcomeMessage.size(), 0);
+
+    std::string dataMessage = "PORT " + dataAddress + "," + std::to_string(dataPort) + "\r\n";
+    send(socket, dataMessage.c_str(), dataMessage.size(), 0);
+}
+
+void sendResponse(int socket, const std::string& response) {
+    send(socket, response.c_str(), response.size(), 0);
+}
+
+std::string listFiles() {
+    std::string filelist;
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(".")) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_REG) {
+                filelist += ent->d_name;
+                filelist += "\r\n";
+            }
+        }
+        closedir(dir);
+    }
+    return filelist;
+}
 
 int main() {
     // Create a socket for the control connection
-    int controlSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int controlSocket = createSocket(2021);
     if (controlSocket == -1) {
-        std::cerr << "Failed to create control socket." << std::endl;
-        return 1;
-    }
-
-    // Set up the server address structure
-    sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(2021);  // FTP control port
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-
-    // Bind the socket to the server address
-    if (bind(controlSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        std::cerr << "Failed to bind control socket." << std::endl;
-        close(controlSocket);
-        return 1;
-    }
-
-    // Listen for incoming connections
-    if (listen(controlSocket, 1) == -1) {
-        std::cerr << "Failed to listen on control socket." << std::endl;
-        close(controlSocket);
         return 1;
     }
 
     std::cout << "Server is listening on port 2021..." << std::endl;
 
     // Accept a client connection
-    sockaddr_in clientAddress{};
-    socklen_t clientAddressLength = sizeof(clientAddress);
-    int controlClientSocket = accept(controlSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
+    int controlClientSocket = acceptClientConnection(controlSocket);
     if (controlClientSocket == -1) {
-        std::cerr << "Failed to accept client connection." << std::endl;
-        close(controlSocket);
         return 1;
     }
 
     std::cout << "Client connected." << std::endl;
 
     // Send welcome message to the client
-    std::string welcomeMessage = "220 Welcome to the FTP server.\r\n";
-    send(controlClientSocket, welcomeMessage.c_str(), welcomeMessage.size(), 0);
-
-    // Send server IP address and port for data connection to the client
-    std::string dataAddress = "127.0.0.1,2022";  // Replace with the actual server IP address and data port
-    std::string dataMessage = "PORT " + dataAddress + "\r\n";
-    send(controlClientSocket, dataMessage.c_str(), dataMessage.size(), 0);
-
+    std::string dataAddress = "127.0.0.1";  // Replace with the actual server IP address
+    int dataPort = 2022;  // Replace with the actual data port
+    sendWelcomeMessage(controlClientSocket, dataAddress, dataPort);
 
     // Create a socket for the data connection
-    int dataSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int dataSocket = createSocket(dataPort);
     if (dataSocket == -1) {
-        std::cerr << "Failed to create data socket." << std::endl;
         return 1;
     }
 
-    // Set up the server address structure for the data connection
-    sockaddr_in dataServerAddress{};
-    dataServerAddress.sin_family = AF_INET;
-    dataServerAddress.sin_port = htons(2022);  // FTP data port
-    dataServerAddress.sin_addr.s_addr = INADDR_ANY;
-
-    // Bind the data socket to the server address
-    if (bind(dataSocket, (struct sockaddr*)&dataServerAddress, sizeof(dataServerAddress)) == -1) {
-        std::cerr << "Failed to bind data socket." << std::endl;
-        close(dataSocket);
-        return 1;
-    }
-
-    // Listen for incoming data connections
-    if (listen(dataSocket, 1) == -1) {
-        std::cerr << "Failed to listen on data socket." << std::endl;
-        close(dataSocket);
-        return 1;
-    }
-
-    std::cout << "Server is listening on port 2022 for data connections..." << std::endl;
+    std::cout << "Server is listening on port " << dataPort << " for data connections..." << std::endl;
 
     // Accept a data connection
-    int dataClientSocket = accept(dataSocket, nullptr, nullptr);
+    int dataClientSocket = acceptClientConnection(dataSocket);
     if (dataClientSocket == -1) {
-        std::cerr << "Failed to accept data connection." << std::endl;
-        close(dataSocket);
         return 1;
     }
 
@@ -105,19 +121,26 @@ int main() {
         ssize_t bytesRead = recv(controlClientSocket, commandBuffer, sizeof(commandBuffer), 0);
         if (bytesRead <= 0) {
             // Error or connection closed
-            std::cout << "Got 0 bytes";
+            std::cout << "Connection closed." << std::endl;
             break;
         }
 
         // Process the command
         std::string command(commandBuffer);
-        // TODO: Implement command processing logic
+        std::string response;
+
+        // LIST command
+        if (command.substr(0, 4) == "LIST") {
+            response = listFiles();
+        } else {
+            // Unknown command
+            response = "Unknown command: " + command + "\r\n";
+        }
 
         // Send response back to the client
-        std::string response = "Command received: " + command + "\r\n";
-        send(controlClientSocket, response.c_str(), response.size(), 0);
+        sendResponse(controlClientSocket, response);
     }
-    
+
     // Close the control client socket and control socket
     close(controlClientSocket);
     close(controlSocket);
