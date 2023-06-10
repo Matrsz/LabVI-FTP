3ee84a8f28bb85d1f1409cc8a5fbf3dea3c38bb5
Matías Roqueta <matias.roqueta.sz@gmail.com>
2023-06-09 21:44:01 -0300

    USER and PASS

--- FTP/Server/server.cpp
+++ FTP/Server/server.cpp
@@ -2,6 +2,40 @@
 #include "filesystem.h"
 #include "commands.h"
 
+std::string handleUSERCommand(int controlClientSocket, const std::string& args) {
+    std::string response;
+    if (args.empty()) {
+        response = "Missing username argument.";
+    } else {
+        std::string username = args;
+        if (username == "admin") {
+            response = "331 User name okay, need password.";
+            sendResponse(controlClientSocket, response);
+            return username;
+        } 
+        response = "530 Not logged in.";
+    }
+    sendResponse(controlClientSocket, response);
+    return ""; // Return empty string if username is not validd
+}
+
+bool handlePASSCommand(int controlClientSocket, const std::string& args, const std::string& username) {
+    std::string response;
+    if (args.empty()) {
+        response = "Missing password argument.";
+    } else {
+        std::string password = args;
+        if (password == "password") {
+            response = "230 User logged in, proceed.";
+            sendResponse(controlClientSocket, response);
+            return true; // Password is correct
+        } 
+        response = "530 Login incorrect.";
+    }
+    sendResponse(controlClientSocket, response);
+    return false; // Password is incorrect
+}
+
 int main() {
     // Create a socket for the control connection
     int controlSocket = createSocket(2021);
@@ -24,6 +58,9 @@ int main() {
     sendWelcomeMessage(controlClientSocket);
 
     // Data connection is now ready for data transfer or other operations
+
+    bool authenticated = false;
+    std::string username = "";
 
     // Enter command loop
     char commandBuffer[1024];
@@ -55,26 +92,40 @@ int main() {
             // Print command details to stdout
             std::cout << "Received command: " << cmd << "\t" << args << std::endl;
 
-            if (cmd == "LIST") {
-                sendResponse(controlClientSocket, listEntries());
-            } else if (cmd == "CWD") {
-                handleCWDCommand(controlClientSocket, args);
-            } else if (cmd == "CDUP") {
-                handleCDUPCommand(controlClientSocket);            
-            } else if (cmd == "MKD") {
-                handleMKDCommand(controlClientSocket, args);
-            } else if (cmd == "RMD") {
-                handleRMDCommand(controlClientSocket, args);
-            } else if (cmd == "DELE") {
-                handleDELECommand(controlClientSocket, args);
-            } else if (cmd == "RETR") {
-                handleRETRCommand(controlClientSocket, args);
-            } else if (cmd == "STOR") {
-                handleSTORCommand(controlClientSocket, args);
-            } else if (cmd == "QUIT") {
-                break;
+            if (!authenticated) {
+                if (cmd == "USER") {
+                    username = handleUSERCommand(controlClientSocket, args);
+                } else if (cmd == "PASS") {
+                    if (username == "") {
+                        sendResponse(controlClientSocket, "Enter valid username first");
+                    } else {
+                        authenticated = handlePASSCommand(controlClientSocket, args, username);
+                    }
+                } else {
+                    sendResponse(controlClientSocket, "Not signed in.");
+                }
             } else {
-                sendResponse(controlClientSocket, "Unsupported command.");
+                if (cmd == "LIST") {
+                    sendResponse(controlClientSocket, listEntries());
+                } else if (cmd == "CWD") {
+                    handleCWDCommand(controlClientSocket, args);
+                } else if (cmd == "CDUP") {
+                    handleCDUPCommand(controlClientSocket);            
+                } else if (cmd == "MKD") {
+                    handleMKDCommand(controlClientSocket, args);
+                } else if (cmd == "RMD") {
+                    handleRMDCommand(controlClientSocket, args);
+                } else if (cmd == "DELE") {
+                    handleDELECommand(controlClientSocket, args);
+                } else if (cmd == "RETR") {
+                    handleRETRCommand(controlClientSocket, args);
+                } else if (cmd == "STOR") {
+                    handleSTORCommand(controlClientSocket, args);
+                } else if (cmd == "QUIT") {
+                    break;
+                } else {
+                    sendResponse(controlClientSocket, "Unsupported command.");
+                }
             }
         }
     }
